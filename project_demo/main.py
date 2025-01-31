1847bdfaf6f72854ce33f523454cc0efc41b1d95
hadjuse <rabearimananahadjpro@gmail.com>
2025-01-31 22:10:53 +0100

    resolve dependencies

--- project_demo/main.py
+++ project_demo/main.py
@@ -12,10 +12,10 @@ import os
 from langsmith import Client
 import tempfile
 import pandas as pd
-
-importlib.import_module('pysqlite3')
 import sys
-sys.modules['sqlite3'] = __import__('pysqlite3')
+__import__('pysqlite3')
+import pysqlite3
+sys.modules['sqlite3'] = sys.modules["pysqlite3"]
 import chromadb
 from langchain_community.vectorstores import Chroma
 # Début du CSS personnalisé
